5348bb57d50cd9204fc90fe2a7bd50a4da69d6b9
HeDu26 <hec.itp@hotmail.com>
2022-12-05 17:36:33 -0600

    Agregando README:md

---
+++ README.md
@@ -0,0 +1,43 @@
+# **_TIPS_** para mejorar lógica de programación
+
+## 1. Descomponer problemas en problemas más pequeños.
+
+## 2. Busca una solución que acepte todos los casos posibles.
+
+## 3. Busca siempre problemas más díficiles.
+
+## 4. Practicar mucho
+
+Plataformas para buscar desafios :
+
+- CoderByte
+- Project Euler
+- **HackerRank**
+- CodeChef
+- Exercism.10
+- Codewars
+
+## 5.
+
+| Estructura de datos | ≠   | Algoritmos básicos |
+| ------------------- | --- | ------------------ |
+| Arreglos            |     | Ordenaminto        |
+| Tablas hash         |     | Heración           |
+| Listas enlazadas    |     | Programación       |
+| Pilas / colas       |     | Búsqueda           |
+| Árboles             |     | Hashing            |
+| Grafos              |     | Dinámica           |
+|                     |     | Parseo y watching  |
+
+## 6. Patrones de diseño
+
+## 7.Mirar código de otra gente
+
+## 8. Crear funciones desde cero (Evitar usar las nativas)
+
+# Static Site Generator (SSG)
+
+Para blogs, portafolios, landing page, etc.
+
+- Astro
+- Hugo
---
+++ js-code/asincronia.js
@@ -0,0 +1,138 @@
+/* //Código síncrono bloqueante
+
+(() => {
+  console.log("Código síncrono");
+  console.log("inicio");
+
+  function dos() {
+    console.log("Dos");
+  }
+
+  function uno() {
+    console.log("Uno");
+    dos();
+    console.log("Tres");
+  }
+
+  uno();
+  console.log("Fin");
+})();
+console.clear();
+//Código asíncrono no bloqueante
+
+(() => {
+  console.log("Código Asíncrono");
+  console.log("Inicio");
+
+  function dos() {
+    setTimeout(function () {
+      console.log("Dos");
+    }, 1000);
+  }
+  function uno() {
+    setTimeout(function () {
+      console.log("Uno");
+    }, 0);
+    dos();
+    console.log("Tres");
+  }
+  uno();
+  console.log("Fin");
+})();
+ */
+
+//CALLBACK
+
+/* function cuadradocallback(value, callback) {
+  setTimeout(() => {
+    callback(value, value * value);
+  }, 0 | (Math.random() * 1000));
+}
+
+cuadradocallback(1, (value, result) => {
+  console.log("inicia callback");
+  console.log(`callback: ${value}, ${result}`);
+  cuadradocallback(2, (value, result) => {
+    console.log(`callback: ${value}, ${result}`);
+    cuadradocallback(3, (value, result) => {
+      console.log(`callback: ${value}, ${result}`);
+      cuadradocallback(4, (value, result) => {
+        console.log(`callback: ${value}, ${result}`);
+        cuadradocallback(5, (value, result) => {
+          console.log(`callback: ${value}, ${result}`);
+          console.log("Fin callback");
+          console.log("Callback hell!!!!😈🤘");
+          console.log("http://callbackhell.com/");
+        });
+      });
+    });
+  });
+}); */
+
+//PROMESAS
+
+/* function cuadradoPromise(value) {
+  if (typeof value !== "number")
+    return Promise.reject(
+      `Error, el valor "${value}" ingresado no es un número`
+    );
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        value,
+        result: value * value,
+      });
+    }, 0 | (Math.random() * 1000));
+  });
+}
+
+cuadradoPromise(0)
+  .then((obj) => {
+    //console.log(obj);
+    console.log("Inicio Promise");
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(1);
+  })
+  .then((obj) => {
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(2);
+  })
+  .then((obj) => {
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(3);
+  })
+  .then((obj) => {
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(4);
+  })
+  .then((obj) => {
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(5);
+  })
+  .then((obj) => {
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    console.log("Fin promise");
+  })
+  .catch(); */
+
+//Funciones asíncronas
+
+function cuadradoPromise(value) {
+  if (typeof value !== "number")
+    return Promise.reject(
+      `Error, el valor "${value}" ingresado no es un número`
+    );
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        value,
+        result: value * value,
+      });
+    }, 0 | (Math.random() * 1000));
+  });
+}
+
+async function functionAsincronaDeclarada() {
+  try {
+  } catch (error) {}
+}
